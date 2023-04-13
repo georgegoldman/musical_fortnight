afbc38f1849a935f0be35ddf35d99a2030e9f913
georgegoldman <georgegoldmanjohn.o@gmail.com>
2023-04-13 18:03:11 +0100

    model done.

--- lib/src/domain/models/trash.dart
+++ lib/src/domain/models/trash.dart
@@ -1,15 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Trash {
-  final String description;
+  final String? description;
 
-  Trash(this.description);
+  Trash({this.description});
 
   factory Trash.fromFireStore(
     DocumentSnapshot<Map<String, dynamic>> snapshot,
     SnapshotOptions? options,
   ) {
     final data = snapshot.data();
-    
+    return Trash(description: data?['description']);
+  }
+
+  Map<String, dynamic> toFireStore() {
+    return {if (description != null) "description": description};
   }
 }
